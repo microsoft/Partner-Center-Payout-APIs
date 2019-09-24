c545830a34f5b22f82f1c1b6e87a9572311167a1
Ishani Shailesh Parikh <ishpari@microsoft.com>
2019-09-24 11:51:46 -0700

    Remove random requestId

--- src/PartnerCenterPayoutAPIsSampleCode/Program.cs
+++ src/PartnerCenterPayoutAPIsSampleCode/Program.cs
@@ -24,8 +24,8 @@ namespace PartnerCenterPayoutAPISampleCode
             HttpResponseMessage transactionHistoryResponse = TransactionHistory.CreateRequest(accessToken);
             PrintResponse(transactionHistoryResponse);
 
-            // Get the export files for a previously created request's requestId.
-            var transactionHistoryRequestId = "3c150cae-11ce-412b-a6bd-29a946471841";
+            // Get the export files for a previously created request's requestId (Guid).
+            var transactionHistoryRequestId = "<add_transactionhistory_request_id_here>";
             HttpResponseMessage transactionHistoryStatusResponse = TransactionHistory.GetRequest(accessToken, transactionHistoryRequestId);
             string transactionHistoryBlobLocation = JObject.Parse(transactionHistoryStatusResponse.Content.ReadAsStringAsync().Result)["value"][0]["blobLocation"].ToString();
             PrintResponse(transactionHistoryStatusResponse);
@@ -41,8 +41,9 @@ namespace PartnerCenterPayoutAPISampleCode
             HttpResponseMessage paymentsResponse = Payments.CreateRequest(accessToken);
             PrintResponse(paymentsResponse);
 
-            // Get the export files for a previously created request's requestId.
-            var paymentsRequestId = "a2c409ec-2109-4470-9b69-208c41ed10fb";
+            // Get the export files for a previously created request's requestId (Guid).
+            var paymentsRequestId = "<add_payments_request_id_here>";
+            
             HttpResponseMessage paymentsStatusResponse = Payments.GetRequest(accessToken, paymentsRequestId);
             string paymentsBlobLocation = JObject.Parse(paymentsStatusResponse.Content.ReadAsStringAsync().Result)["value"][0]["blobLocation"].ToString();
             PrintResponse(paymentsStatusResponse);
