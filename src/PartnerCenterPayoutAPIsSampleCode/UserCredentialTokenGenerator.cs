b9ea2eefae38212448f22b748cdd6f97e66dbe82
Scott Seely <scseely@microsoft.com>
2021-04-10 17:06:13 -0700

    Update the code to not require a password in configuration.
    This version uses MSAL and uses a Device Code to access. This allows the sample to work with MFA.
    
    The sample also demonstrates how to renew the user token using MSAL.

--- src/PartnerCenterPayoutAPIsSampleCode/UserCredentialTokenGenerator.cs
+++ src/PartnerCenterPayoutAPIsSampleCode/UserCredentialTokenGenerator.cs
@@ -1,47 +1,72 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace PartnerCenterPayoutAPISampleCode
-{
-    using System.Configuration;
-    using System.Net.Http;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 
+namespace PartnerCenterPayoutAPIsSampleCode
+{
     public class UserCredentialTokenGenerator
     {
-        public static async Task<string> GetAccessToken()
+        private static string TenantId => ConfigurationManager.AppSettings.Get("tenantId");
+        private static string ClientId => ConfigurationManager.AppSettings.Get("clientId");
+        private static IEnumerable<string> Scopes => new[] {"https://api.partner.microsoft.com/.default"};
+
+        private IPublicClientApplication _publicClientApplication = null;
+
+        public async Task Init()
         {
-            // Fetch these settings the App config key values
-            string tenantId = ConfigurationManager.AppSettings.Get("tenantId");
-            string clientId = ConfigurationManager.AppSettings.Get("clientId");
-            string userName = ConfigurationManager.AppSettings.Get("userName");
-            string password = ConfigurationManager.AppSettings.Get("password");
-            string scope = "https://api.partner.microsoft.com";
-
-            string tokenEndpointFormat = "https://login.microsoftonline.com/{0}/oauth2/token";
-            string tokenEndpoint = string.Format(tokenEndpointFormat, tenantId);
-
-            dynamic result;
-            using (HttpClient client = new HttpClient())
+            if (_publicClientApplication != null)
             {
-                string tokenUrl = tokenEndpoint;
-                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, tokenUrl))
-                {
-                    string content = string.Format("grant_type=password&username={0}&password={1}&client_id={2}&resource={3}", userName, password, clientId, scope);
-
-                    request.Content = new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded");
-
-                    using (HttpResponseMessage response = await client.SendAsync(request))
-                    {
-                        string responseContent = await response.Content.ReadAsStringAsync();
-                        result = JsonConvert.DeserializeObject(responseContent);
-                    }
-                }
+                return;
             }
 
-            return result.access_token;
+            var options = new PublicClientApplicationOptions
+            {
+                TenantId = TenantId, 
+                ClientId = ClientId, 
+                AzureCloudInstance = AzureCloudInstance.AzurePublic
+            };
+
+            _publicClientApplication = PublicClientApplicationBuilder.CreateWithApplicationOptions(options).Build();
+            var builder = _publicClientApplication.AcquireTokenWithDeviceCode(Scopes, DeviceCodeResultCallback);
+            var result = await builder.ExecuteAsync();
+        }
+
+        /// <summary>
+        /// GetToken makes sure that the token is renewed on every fetch.
+        /// If a number of authenticated calls will happen within a one minute time span, feel free
+        /// to reuse the value returned here. It is also acceptable to call the method when starting
+        /// new work. By default, tokens returned from here will be valid for 3600 seconds (1 hour).
+        /// </summary>
+        /// <returns>
+        /// The AuthenticationResult returns the one token used for authen
+        /// </returns>
+        public async Task<AuthenticationResult> GetToken()
+        {
+            await Init();
+
+            var account = (await _publicClientApplication.GetAccountsAsync()).FirstOrDefault();
+            if (account == null)
+            {
+                throw new InvalidCredentialException("It appears that the application has failed to authenticate to AAD.");
+            }
+
+            var result = await _publicClientApplication.AcquireTokenSilent(Scopes, account).WithForceRefresh(true)
+                .ExecuteAsync();
+
+            return result;
+        }
+
+        private Task DeviceCodeResultCallback(DeviceCodeResult arg)
+        {
+            Console.WriteLine($"Please go to {arg.VerificationUrl} and use this Device Code: {arg.UserCode}");
+            return Task.Delay(0);
         }
     }
 }
